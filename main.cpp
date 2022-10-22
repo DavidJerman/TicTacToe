b2dc786398b758bff0541a40929797698423cbc3
David <45101011+DavidJerman@users.noreply.github.com>
2022-10-22 14:03:20 +0200

    Added the game main menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+void greetUser() {
+    std::cout << "Welcome to the Tic-Tac-Toe game!" << std::endl;
+}
+
+void printMainMenu() {
+    std::cout << "=============================="
+              << "1) New game\n"
+              << "2) About the game\n"
+              << "3) Exit"
+              << "=============================="
+              << std::endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char in;
+
+    greetUser();
+    printMainMenu();
+
+    // Main game loop
+    while (std::cin >> in) {
+
+
+        printMainMenu();
+    }
     return 0;
 }
